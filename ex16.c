d8ea508b85c892978d1a104c2ab92797d9d2ea9e
Jamie White <jamie@jgwhite.co.uk>
2015-09-15 08:23:14 +0100

    ex16 extra credit

--- ex16.c
+++ ex16.c
@@ -18,52 +18,42 @@ struct Person {
 };
 
 // Person_create function
-struct Person *Person_create(char *name, int age, int height, int weight)
+struct Person Person_create(char *name, int age, int height, int weight)
 {
 	// Allocate enough memory to hold the new person
-	struct Person *who = malloc(sizeof(struct Person));
-	// Assert the memory was definitely allocated and
-	// we have a valid pointer
-	assert(who != NULL);
+	struct Person who;
 
 	// Copy the name arguments and write it to the person's name field
-	who->name = strdup(name);
+	who.name = strdup(name);
 	// Write age, height and weight to their respective fields
-	who->age = age;
-	who->height = height;
-	who->weight = weight;
+	who.age = age;
+	who.height = height;
+	who.weight = weight;
 
 	return who;
 }
 
-// Person_destroy function
-void Person_destroy(struct Person *who)
+void Person_destroy(struct Person who)
 {
-	// Assert we have a valid pointer
-	assert(who != NULL);
-
-	// Free the name string
-	free(who->name);
-	// Free the person struct
-	free(who);
+	free(who.name);
 }
 
 // Person_print function
-void Person_print(struct Person *who)
+void Person_print(struct Person who)
 {
-	printf("Name: %s\n", who->name);
-	printf("\tAge: %d\n", who->age);
-	printf("\tHeight: %d\n", who->height);
-	printf("\tWeight: %d\n", who->weight);
+	printf("Name: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tHeight: %d\n", who.height);
+	printf("\tWeight: %d\n", who.weight);
 }
 
 int main(int argc, char *argv[])
 {
 	// make two people structures
-	struct Person *joe = Person_create(
+	struct Person joe = Person_create(
 			"Joe Alex", 32, 64, 100);
 
-	struct Person *frank = Person_create(
+	struct Person frank = Person_create(
 			"Frank Blank", 20, 72, 180);
 
 	// print them out and where they are in memory
@@ -74,13 +64,13 @@ int main(int argc, char *argv[])
 	Person_print(frank);
 
 	// make everyone age 20 years and print them again
-	joe->age += 20;
-	joe->height -= 2;
-	joe->weight += 40;
+	joe.age += 20;
+	joe.height -= 2;
+	joe.weight += 40;
 	Person_print(joe);
 
-	frank->age += 20;
-	frank->weight += 20;
+	frank.age += 20;
+	frank.weight += 20;
 	Person_print(frank);
 
 	// destroy them both so we clean up
